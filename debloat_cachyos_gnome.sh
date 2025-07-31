4e05ae77f5cec4d15790914f04344e54f537d643
amonetlol <65382640+amonetlol@users.noreply.github.com>
2025-07-31 00:57:21 -0300

    Create debloat_cachyos_gnome.sh

---
+++ debloat_cachyos_gnome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Gnome
+sudo pacman -Rns --noconfirm octopi alacritty meld nano-syntax-highlighting micro gedit gnome-terminal gnome-music simple-scan snapshot gnome-software gnome-weather gnome-contacts gnome-maps decibels malcontent gnome-tour epiphany gnome-calendar
+
+# CachyOS
+sudo pacman -Rns --noconfirm cachyos-grub-theme cachyos-hello cachyos-kernel-manager cachyos-packageinstaller cachyos-plymouth-bootanimation cachyos-wallpapers cachyos-micro-settings
